95a213881d79d2e3e5f9f18789cc318e27fc0574
Claudio Altamura <claudio@claudioaltamura.de>
2022-03-02 13:39:28 +0100

    new: import example data

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,2 @@
+INSERT INTO superheroes (name, real_name, power) VALUES ('Spider-Man', 'Peter Parker', 90.0);
+INSERT INTO superheroes (name, real_name, power) VALUES ('Hulk', 'Bruce Banner', 100.0);
