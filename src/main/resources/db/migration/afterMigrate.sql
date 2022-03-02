e56b40b153416ecc2bd6c9d08c446fb79c1f4d0b
Claudio Altamura <claudio@claudioaltamura.de>
2022-03-02 13:59:38 +0100

    fix: duplicate key error

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -1,2 +1,2 @@
-INSERT INTO superheroes (name, real_name, power) VALUES ('Spider-Man', 'Peter Parker', 90.0);
-INSERT INTO superheroes (name, real_name, power) VALUES ('Hulk', 'Bruce Banner', 100.0);
+INSERT INTO superheroes (name, real_name, power) VALUES ('Spider-Man', 'Peter Parker', 90.0) ON CONFLICT DO NOTHING;
+INSERT INTO superheroes (name, real_name, power) VALUES ('Hulk', 'Bruce Banner', 100.0) ON CONFLICT DO NOTHING;
